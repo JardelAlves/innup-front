131b8f043735bfcb36833713cddf3a1da79bbe4f
Jardel <jardelfilho2001@gmail.com>
2019-12-03 15:23:40 -0300

    Inicializando o Footer

---
+++ vue-innup/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-footer color="primary" absolute padless>
+    <v-row>
+      <v-col order="first">
+        <v-card flat>
+          <v-card-text class="justify-center">
+            <v-icon x-large @click="$emit()">mdi-home</v-icon>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+}
+</script>
