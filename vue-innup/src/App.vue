d362494d3917e0701630bbcdc696af6c5f800240
Jardel <jardelfilho2001@gmail.com>
2019-12-04 13:49:13 -0300

    Mais mudanças

--- vue-innup/src/App.vue
+++ vue-innup/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
+    <Toolbar/>
     <Footer/>
   </div>
 </template>
 
 <script>
 import Footer from './components/Footer';
+import Toolbar from './components/Toolbar';
 
 export default {
   name: 'App',
 
   components: {
     Footer,
+    Toolbar,
   },
 };
 </script>
--- vue-innup/src/components/Footer.vue
+++ vue-innup/src/components/Footer.vue
@@ -1,15 +1,29 @@
 <template>
   <v-footer color="primary" absolute padless>
-    <v-row>
-      <v-col order="first">
-        <v-card flat>
-          <v-card-text class="justify-center">
-            <v-icon x-large @click="$emit()">mdi-home</v-icon>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      
-    </v-row>
+    <v-card width="100%" >
+      <v-container fluid>
+        <v-row align-content="center" justify="center">
+          <v-col align-self="center">
+            <v-btn icon>
+              <v-icon large>mdi-home</v-icon>
+            </v-btn>
+          </v-col>
+
+          <v-col align-self="center">
+            <v-btn icon>
+              <v-icon large>mdi-magnify</v-icon>
+            </v-btn>
+          </v-col>
+
+          <v-col align-self="center">
+            <v-btn icon>
+              <v-icon large>mdi-account</v-icon>
+            </v-btn>
+          </v-col>
+
+        </v-row>
+      </v-container>
+    </v-card>
   </v-footer>
 </template>
 
